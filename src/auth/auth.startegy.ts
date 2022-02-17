a39808de02094cd747eec976e7b6ad0b3d174ce6
Linar <a_linar94@mail.ru>
2022-02-17 12:19:04 +0300

    added users module and get many request

--- src/auth/auth.startegy.ts
+++ src/auth/auth.startegy.ts
@@ -1,20 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-local';
 import { AuthService } from './auth.service';
+import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayload } from './auth.types';
 
 @Injectable()
-export class AuthStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    private readonly authService: AuthService,
-    private readonly configService: ConfigService,
-  ) {
-    super({ secretOrKey: configService.get<string>('jwt.secret') });
+export class AuthStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(private readonly authService: AuthService, readonly configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: configService.get<string>('jwt.secret'),
+    });
   }
 
   async validate(payload: JwtPayload) {
-    return await this.authService.validateJwtToken(payload);
+    return this.authService.validateJwtToken(payload);
   }
 }
